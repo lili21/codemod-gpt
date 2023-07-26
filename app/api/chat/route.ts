a31eddcba8d92fbc668039eb622003ae456a8983
li.li <mark.li210812@gmail.com>
2023-07-26 23:04:44 +0800

    remove unstable_allowDynamic

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -6,11 +6,6 @@ const openai = new OpenAI({
 
 export const runtime = 'edge'
 
-export const unstable_allowDynamic = [
-  // This is currently required because `qs` uses `side-channel` which depends on this.
-  '/node_modules/function-bind/**',
-]
-
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
   const formData = await req.formData()
